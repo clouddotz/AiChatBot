6bf6740bd93b60aec6b61307c70bf7ca74636f6b
topgyshon <150448310+topgyshon@users.noreply.github.com>
2024-04-19 16:38:36 +0100

    Started UI

---
+++ src/components/sideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SideBar.css'
+
+const SideBar = ()=>{
+  
+}
+
+
+
+
+export default SideBar
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SideBar from '../components/sideBar/SideBar'
+import "./mainlayout.css"
+
+const MainLayout = () => {
+  return (
+    <div id='layout'>
+        <div id='side-nav'>
+          <SideBar/>
+        </div>
+     
+
+    </div>
+  )
+}
+
+export default MainLayout
