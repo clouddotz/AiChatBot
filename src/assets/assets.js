edb242db3fa2c3e9ef1fdfee46beac4556cf377a
Abefe <lukmonakingbade@gmail.com>
2024-04-23 07:05:34 +0100

    renaming icons

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,12 +7,12 @@ import bulb_icon from './bulb_icon.png'
 import compass_icon from './compass_icon.png'
 import gallery_icon from './gallery_icon.png'
 import mic_icon from './mic_icon.png'
-import user_icon from './user_icon.png'
+import user_icon from './user_icon.jpeg'
 import youtube_icon from './youtube_icon.png'
 import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
-import gemini_icon from './gemini_icon.png'
+import sail_logo from './sail_logo.jpeg'
 
 export const assets = {
     history_icon,
