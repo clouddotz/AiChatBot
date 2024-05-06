f491838c2aa18c376ffce3158987535e53220bfe
Project SURVIVOR <161699187+olawaleraji@users.noreply.github.com>
2024-05-06 16:39:59 +0100

    check this out

--- server/client/src/App.jsx
+++ server/client/src/App.jsx
@@ -1,13 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 // import MainLayout from './layout/MainLayout'
 import Router from './Router'
+import 'tailwindcss/tailwind.css';
 
 function App() {
   
 
   return (
     <>
-      
       {/* <MainLayout/> */}
       <RouterProvider router={Router}/>
 
